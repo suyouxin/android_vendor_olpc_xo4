8c03f7df065d9f0cd7dfeb13efd2e7c9fbabc33a
Youxin Su <suyouxin@gmail.com>
2014-08-19 08:11:54 +1200

    increasing vm heap size to 256M in order to run large 3D game

--- device-vendor.mk
+++ device-vendor.mk
@@ -74,8 +74,8 @@ PRODUCT_PACKAGES += \
 # system property sets
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=5m \
-    dalvik.vm.heapgrowthlimit=48m \
-    dalvik.vm.heapsize=128m \
+    dalvik.vm.heapgrowthlimit=64m \
+    dalvik.vm.heapsize=256m \
     ro.sf.lcd_density=200 \
     ro.phone.enabled=false \
 	ro.carrier=wifi-only \
